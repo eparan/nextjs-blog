9c754c1aa0e28780bf7d05b1b65f00f23c04b1ac
eparan <eparan@gmail.com>
2023-03-02 20:19:28 +0900

    bug fix

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Layout from "../../components/layout";
+import Head from "next/head";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Date from "../../components/date";
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
   return (
