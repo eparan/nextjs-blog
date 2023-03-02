76c3189be561d2bca3f219947d09ecd901e06433
eparan <eparan@gmail.com>
2023-03-02 20:24:26 +0900

    bugfix

--- components/date.js
+++ components/date.js
@@ -2,8 +2,6 @@ import { parseISO, format } from 'date-fns';
 
 export default function Date ({ dateString }) {
   const date = parseISO(dateString);
-  return '시간
-  
-  '
+  return '시간';
   // return <time dateTime={dateString}>{parseISO(dateString)}</time>;
 }
